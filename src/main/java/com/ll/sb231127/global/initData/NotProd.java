eef98cd938230ab9b6112d6b999ad54b10678637
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-29 02:11:14 +0900

    step 18, @Autowired @Lazy private NotProd self; 을 통해서 내부 호출이라도 @Transactional 을 작동시킬 수 있다.

--- src/main/java/com/ll/sb231127/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -4,51 +4,55 @@ import com.ll.sb231127.domain.article.article.entity.Article;
 import com.ll.sb231127.domain.article.article.service.ArticleService;
 import com.ll.sb231127.domain.member.member.entity.Member;
 import com.ll.sb231127.domain.member.member.service.MemberService;
-import org.springframework.boot.ApplicationArguments;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Profile;
 import org.springframework.transaction.annotation.Transactional;
 
 @Profile("!prod")
 @Configuration
+@RequiredArgsConstructor
 public class NotProd {
     // 개발/테스트 샘플 데이터
+
+    @Autowired
+    @Lazy // 본인이 본인 의존성 얻을려다가 뻗는다.
+    private NotProd self; // 자신에 대한 객체, 많이 사용하는 패턴
+    private final MemberService memberService;
+    private final ArticleService articleService;
+
     @Bean
-    public ApplicationRunner initNotProdData(
-            MemberService memberService,
-            ArticleService articleService
-    ) {
-        return new ApplicationRunner() {
-            @Override
-            public void run(ApplicationArguments args) {
-                work1();
-                work2();
-            }
-
-            @Transactional
-            public void work1() {
-                // 사용자 생성
-                Member member1 = memberService.join("user1", "1234").getData();
-                Member member2 = memberService.join("user2", "1234").getData();
-
-                // 게시글 생성
-                Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
-                Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
-                Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
-                Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
-            }
-
-            @Transactional
-            public void work2() {
-                Member member1 = memberService.findById(1L).get();
-                Article article1 = articleService.findById(1L).get();
-
-                // 게시글에서 댓글 저장
-                article1.addComment(member1, "댓글4");
-                article1.addComment(member1, "댓글4");
-            }
+    public ApplicationRunner initNotProdData() {
+        return args -> {
+            self.work1();
+            self.work1();
         };
+    }
+
+    @Transactional
+    public void work1() {
+        // 사용자 생성
+        Member member1 = memberService.join("user1", "1234").getData();
+        Member member2 = memberService.join("user2", "1234").getData();
+
+        // 게시글 생성
+        Article article1 = articleService.write(member1.getId(), "제목1", "내용1").getData();
+        Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
+        Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
+        Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
+    }
+
+    @Transactional
+    public void work2() {
+        Member member1 = memberService.findById(1L).get();
+        Article article1 = articleService.findById(1L).get();
+
+        // 게시글에서 댓글 저장
+        article1.addComment(member1, "댓글4");
+        article1.addComment(member1, "댓글4");
     }
 }
