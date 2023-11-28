7c942a2df8d3616f24a5b5fbd9383ff7de8beb7a
Son-Gyeongi <thsruddl77@gmail.com>
2023-11-29 00:53:54 +0900

    step 10, @CreatedDate (을)를 도입하여 객체 저장(INSERT)시에 createDate 를 수동으로 갱신할 필요가 없어짐

--- src/main/java/com/ll/sb231127/Sb231127Application.java
+++ src/main/java/com/ll/sb231127/Sb231127Application.java
@@ -2,8 +2,11 @@ package com.ll.sb231127;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing // @CreatedDate, @LastModifiedDate를 사용하기 위해 필요
+// JPA Auditing은 엔티티의 생성일(created date)과 수정일(modified date)을 자동으로 관리하는 기능을 제공
 public class Sb231127Application {
 
     public static void main(String[] args) {
--- src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
+++ src/main/java/com/ll/sb231127/global/jpa/baseEntity/BaseEntity.java
@@ -1,16 +1,16 @@
 package com.ll.sb231127.global.jpa.baseEntity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.*;
 import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 // 모든 엔티티가 가져야할 공통 속성을 모아두는 클래스 BaseEntity 도입
 @MappedSuperclass // 부모 클래스로 사용되는 것을 나타낸다, 매핑 정보 상속
+@EntityListeners(AuditingEntityListener.class) // @CreatedDate, @LastModifiedDate를 사용하기 위해 필요
 @SuperBuilder // 부모 클래스인 BaseEntity에서도 빌더 패턴을 사용할 수 있다.
 @NoArgsConstructor(access = AccessLevel.PROTECTED) // 매개변수가 없는 protected 접근 제한자를 가지는 기본 생성자를 자동으로 생성
 @Getter // 필드에 대한 getter 메서드가 자동으로 생성
@@ -21,6 +21,7 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // 엔티티의 식별자 값을 자동으로 생성
     @EqualsAndHashCode.Include // 이 어노테이션이 적용된 필드는 equals()와 hashCode() 메서드에서 동등성 비교에 사용
     private Long id;
+    @CreatedDate // 해당 필드의 값은 엔티티가 생성될 때 자동으로 설정 (JPA Auditing)
     private LocalDateTime createDate;
     private LocalDateTime modifyDate;
 }
