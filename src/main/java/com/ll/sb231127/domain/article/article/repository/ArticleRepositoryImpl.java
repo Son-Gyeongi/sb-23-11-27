3f65877bbc3d07dbccca20c29ed8576449660ac3
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 21:21:48 +0900

    step 30, LEFT JOIN 으로 변경

--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepositoryImpl.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepositoryImpl.java
@@ -1,6 +1,9 @@
 package com.ll.sb231127.domain.article.article.repository;
 
 import com.ll.sb231127.domain.article.article.entity.Article;
+import com.ll.sb231127.domain.article.articleComment.entity.QArticleComment;
+import com.ll.sb231127.domain.article.articleTag.entity.QArticleTag;
+import com.ll.sb231127.domain.member.member.entity.QMember;
 import com.querydsl.core.BooleanBuilder;
 import com.querydsl.core.types.Order;
 import com.querydsl.core.types.OrderSpecifier;
@@ -27,12 +30,17 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
     public Page<Article> search(List<String> kwTypes, String kw, Pageable pageable) {
         BooleanBuilder builder = new BooleanBuilder();
 
+        QMember author = new QMember("articleAuthor");
+        QArticleTag articleTag = new QArticleTag("articleTag");
+        QArticleComment comment = new QArticleComment("articleComment");
+        QMember commentAuthor = new QMember("articleCommentAuthor");
+
         if (!kw.isBlank()) {
             // 기존의 조건을 리스트에 담습니다.
             List<BooleanExpression> conditions = new ArrayList<>();
 
             if (kwTypes.contains("authorUsername")) {
-                conditions.add(article.author.username.containsIgnoreCase(kw));
+                conditions.add(author.username.containsIgnoreCase(kw));
             }
 
             if (kwTypes.contains("title")) {
@@ -44,15 +52,15 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
             }
 
             if (kwTypes.contains("tagContent")) {
-                conditions.add(article.tags.any().content.eq(kw));
+                conditions.add(articleTag.content.eq(kw));
             }
 
             if (kwTypes.contains("commentAuthorUsername")) {
-                conditions.add(article.comments.any().author.username.containsIgnoreCase(kw));
+                conditions.add(commentAuthor.username.containsIgnoreCase(kw));
             }
 
             if (kwTypes.contains("commentBody")) {
-                conditions.add(article.comments.any().body.containsIgnoreCase(kw));
+                conditions.add(comment.body.containsIgnoreCase(kw));
             }
 
             // 조건 리스트를 or 조건으로 결합합니다.
@@ -67,8 +75,12 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
         }
 
         JPAQuery<Article> articlesQuery = jpaQueryFactory
-                .select(article)
+                .selectDistinct(article)
                 .from(article)
+                .leftJoin(article.author, author)
+                .leftJoin(article.comments, comment)
+                .leftJoin(comment.author, commentAuthor)
+                .leftJoin(article.tags, articleTag)
                 .where(builder);
 
         for (Sort.Order o : pageable.getSort()) {
@@ -80,8 +92,12 @@ public class ArticleRepositoryImpl implements ArticleRepositoryCustom {
         articlesQuery.offset(pageable.getOffset()).limit(pageable.getPageSize());
 
         JPAQuery<Long> totalQuery = jpaQueryFactory
-                .select(article.count())
+                .selectDistinct(article.countDistinct())
                 .from(article)
+                .leftJoin(article.author, author)
+                .leftJoin(article.comments, comment)
+                .leftJoin(comment.author, commentAuthor)
+                .leftJoin(article.tags, articleTag)
                 .where(builder);
 
         return PageableExecutionUtils.getPage(articlesQuery.fetch(), pageable, totalQuery::fetchOne);
