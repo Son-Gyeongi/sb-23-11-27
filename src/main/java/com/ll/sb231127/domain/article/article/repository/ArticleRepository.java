55cb5c793957225079ecc455c28bbe5a8e994295
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 19:05:44 +0900

    step 11, 순수 JPA 메서드로 검색구현, 구현한 조건들 : 제목, 내용, 제목 or 내용

--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
@@ -1,10 +1,17 @@
 package com.ll.sb231127.domain.article.article.repository;
 
 import com.ll.sb231127.domain.article.article.entity.Article;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByOrderByIdDesc();
+
+    // 순수 JPA 메서드로 검색구현, 구현한 조건들 : 제목, 내용, 제목 or 내용
+    Page<Article> findByTitleContainingOrBodyContaining(String kw, String kw_, Pageable pageable);
+    Page<Article> findByTitleContaining(String kw, Pageable pageable);
+    Page<Article> findByBodyContaining(String kw, Pageable pageable);
 }
