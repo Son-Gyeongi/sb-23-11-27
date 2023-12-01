67f7265fccdfc0230f4a59eb3978387449a7e453
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 19:34:22 +0900

    step 15, QueryDSL 로 검색구현, 구현한 조건들 : 작성자명 or 제목 or 내용

--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ArticleRepository extends JpaRepository<Article, Long>, ArticleRepositoryCustom {
-    // ArticleRepository 에 QueryDSL 적용 - ArticleRepositoryCustom
+    // ArticleRepository 에 QueryDSL 적용 - 적용했다는 뜻은 ArticleRepositoryCustom, ArticleRepositoryImpl 클래스 추가
 
     List<Article> findByOrderByIdDesc();
 }
