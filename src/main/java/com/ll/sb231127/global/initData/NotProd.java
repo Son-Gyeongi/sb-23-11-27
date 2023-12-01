99f3391d9d04bbb7aa99f7fdd99c0b547e142e5c
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 17:30:38 +0900

    step 3, test 모드와 달리 dev 모드에서는 NotProd 의 샘플 데이터 생성코드가 1번만 실행되도록

--- src/main/java/com/ll/sb231127/global/initData/NotProd.java
+++ src/main/java/com/ll/sb231127/global/initData/NotProd.java
@@ -29,12 +29,13 @@ public class NotProd {
     public ApplicationRunner initNotProdData() {
         return args -> {
             self.work1();
-            self.work2();
         };
     }
 
     @Transactional
     public void work1() {
+        if (memberService.count() > 0) return;
+
         // 사용자 생성
         Member member1 = memberService.join("user1", "1234").getData();
         Member member2 = memberService.join("user2", "1234").getData();
@@ -44,19 +45,11 @@ public class NotProd {
         Article article2 = articleService.write(member1.getId(), "제목2", "내용2").getData();
         Article article3 = articleService.write(member2.getId(), "제목3", "내용3").getData();
         Article article4 = articleService.write(member2.getId(), "제목4", "내용4").getData();
-    }
-
-    @Transactional
-    public void work2() {
-        // 사용자 조회
-        Member member1 = memberService.findById(1L).get();
-        // 게시글 조회
-        Article article1 = articleService.findById(1L).get();
-        Article article2 = articleService.findById(2L).get();
 
         // 게시글에서 댓글 저장
         article1.addComment(member1, "댓글1");
         article1.addComment(member1, "댓글2");
+
         article2.addComment(member1, "댓글3");
         article2.addComment(member1, "댓글4");
         article2.addComment(member1, "댓글5");
