5222ee13a70b603efe922233c59845fc18ee6b14
Son-Gyeongi <thsruddl77@gmail.com>
2023-12-01 19:15:19 +0900

    step 13, 순수 JPA 메서드로 검색구현, 구현한 조건들 : 전체 조건 체크

--- src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
+++ src/main/java/com/ll/sb231127/domain/article/article/repository/ArticleRepository.java
@@ -10,6 +10,8 @@ import java.util.List;
 public interface ArticleRepository extends JpaRepository<Article, Long> {
     List<Article> findByOrderByIdDesc();
 
+    // 순수 JPA 메서드로 검색구현, 구현한 조건들 : 전체 조건 체크
+    Page<Article> findByAuthor_usernameContainingOrTitleContainingOrBodyContainingOrTags_contentOrComments_author_usernameContainingOrComments_bodyContaining(String kw, String kw_, String kw__, String kw___, String kw____, String kw_____, Pageable pageable);
     // 순수 JPA 메서드로 검색구현, 구현한 조건들 : 제목, 내용, 제목 or 내용
     Page<Article> findByTitleContainingOrBodyContaining(String kw, String kw_, Pageable pageable);
     Page<Article> findByTitleContaining(String kw, Pageable pageable);
